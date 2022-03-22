9741e163f0a85c86bfba57b96424ef75d18b009b
Rainer Freiberg <IT-19E-311@tptlive.ee>
2022-03-22 09:36:19 +0200

    Create tund3.py

---
+++ tund3.py
@@ -0,0 +1,32 @@
+print("Game started")
+print("1 2 3")
+print("4 5 6")
+print("7 8 9")
+
+
+ruut = input("P1, sisesta ruut nr:")
+print(ruut)
+if ruut == "1":
+    print("x 2 3")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "2":
+    print("1 x 3")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "3":
+    print("1 2 x")
+    print("4 5 6")
+    print("7 8 9")
+
+if ruut == "4":
+    print("1 2 3")
+    print("x 5 6")
+    print("7 8 9")
+
+if ruut == "5":
+    print("1 2 3")
+    print("4 x 6")
+    print("7 8 9")
